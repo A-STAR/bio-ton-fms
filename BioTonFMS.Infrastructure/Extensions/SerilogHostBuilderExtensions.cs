0d418970dee6bfe9e38d69c298af50a9952cc7d0
levchmike <levchmike@gmail.com>
2022-09-14 19:17:15 +0700

    Add serilog configuring

---
+++ BioTonFMS.Infrastructure/Extensions/SerilogHostBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Core;
+
+namespace BioTonFMS.Infrastructure.Extensions
+{
+    public static class SerilogHostBuilderExtensions
+    {
+        public static WebApplicationBuilder ConfigureSerilog(
+            this WebApplicationBuilder builder)
+        {
+            var logger = new LoggerConfiguration()
+              .ReadFrom.Configuration(builder.Configuration)
+              .Enrich.FromLogContext()
+              .CreateLogger();
+
+            builder.Logging.ClearProviders();
+            builder.Logging.AddSerilog(logger);
+            
+            return builder;
+        }
+    }
+}
