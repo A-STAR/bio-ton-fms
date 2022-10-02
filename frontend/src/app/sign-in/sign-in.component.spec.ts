ce7b2d16abc396b717f5a5ad59f1a8a9d0e816c5
A-STAR <andreysuperstar@gmail.com>
2022-10-02 05:39:03 +0300

    [BIO-22] feat(frontend): add authentication layout
    Add authentication and Sign in component layout.
    
    Reset `mat-card` styles. Style card shadow.
    
    Test card rendering.

--- frontend/src/app/sign-in/sign-in.component.spec.ts
+++ frontend/src/app/sign-in/sign-in.component.spec.ts
@@ -1,10 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatCardHarness } from '@angular/material/card/testing';
 
 import { SignInComponent } from './sign-in.component';
 
 describe('SignInComponent', () => {
   let component: SignInComponent;
   let fixture: ComponentFixture<SignInComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
     await TestBed
@@ -14,6 +18,8 @@ describe('SignInComponent', () => {
       .compileComponents();
 
     fixture = TestBed.createComponent(SignInComponent);
+    loader = TestbedHarnessEnvironment.loader(fixture);
+
     component = fixture.componentInstance;
 
     fixture.detectChanges();
@@ -21,5 +27,13 @@ describe('SignInComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render card', async () => {
+    const cards = await loader.getAllHarnesses(MatCardHarness);
+
+    expect(cards.length)
+      .withContext('render a single card')
+      .toBe(1);
   });
 });
--- frontend/src/app/sign-in/sign-in.component.ts
+++ frontend/src/app/sign-in/sign-in.component.ts
@@ -1,10 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'bio-sign-in',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule
+  ],
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.sass'],
   changeDetection: ChangeDetectionStrategy.OnPush
