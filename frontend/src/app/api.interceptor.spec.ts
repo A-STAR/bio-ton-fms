f60b8e369e4cc408af7baf3c64fbb9db870f8c7b
A-STAR <andreysuperstar@gmail.com>
2022-10-09 14:57:33 +0300

    [BIO-26] feat(frontend): add API interceptor
    Add development and production API path to environments.
    
    Add `APIInterceptor` to prepend API path to request url.
    
    Test HTTP request URL modification to API URL.

---
+++ frontend/src/app/api.interceptor.spec.ts
@@ -0,0 +1,52 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+import { APIInterceptor } from './api.interceptor';
+
+import { environment } from 'src/environments/environment';
+
+describe('APIInterceptor', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [
+      {
+        provide: HTTP_INTERCEPTORS,
+        useClass: APIInterceptor,
+        multi: true
+      },
+      APIInterceptor
+    ]
+  }));
+
+  it('should be created', () => {
+    const interceptor = TestBed.inject(APIInterceptor);
+
+    expect(interceptor).toBeTruthy();
+  });
+
+  it('should modify HTTP request URL to API URL', (done: DoneFn) => {
+    const httpClient = TestBed.inject(HttpClient);
+    const httpTestingController = TestBed.inject(HttpTestingController);
+
+    const testRequestURL = '/';
+
+    httpClient
+      .get<string>(testRequestURL)
+      .subscribe(() => {
+        done();
+      });
+
+    const apiRequestURL = `${environment.api}${testRequestURL}`;
+
+    const testRequest = httpTestingController.expectOne(apiRequestURL, 'test API request');
+
+    expect(testRequest.request.url)
+      .withContext('prepend API to HTTP request URL')
+      .toBe(apiRequestURL)
+
+    testRequest.flush(null);
+
+    httpTestingController.verify();
+  });
+});
