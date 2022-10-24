31475cfc1e4ab649de392703a5776da15944f1f2
levchmike <levchmike@gmail.com>
2022-10-24 22:22:42 +0700

    [BIO-44] add enum methods

--- BioTonFMS.Domain/TrackerTypeEnum.cs
+++ BioTonFMS.Domain/TrackerTypeEnum.cs
@@ -1,13 +1,31 @@
-﻿namespace BioTonFMS.Domain
+﻿using System.ComponentModel;
+
+namespace BioTonFMS.Domain
 {
     /// <summary>
     /// Тип протокола, поддерживаемый трекером
     /// </summary>
     public enum TrackerTypeEnum
     {
+        /// <summary>
+        /// Протокол GalileoSkyV50
+        /// </summary>
+        [Description("Протокол GalileoSkyV50")]
         GalileoSkyV50,
+        /// <summary>
+        /// Протокол WialonIPS
+        /// </summary>
+        [Description("Протокол WialonIPS")]
         WialonIPS,
+        /// <summary>
+        /// Протокол Wialon
+        /// </summary>
+        [Description("Протокол Wialon")]
         Wialon,
+        /// <summary>
+        /// Протокол Retranslator
+        /// </summary>
+        [Description("Протокол Retranslator")]
         Retranslator
     }
 }
