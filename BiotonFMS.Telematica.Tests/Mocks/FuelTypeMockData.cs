b9c44ec1ad4d4ff3a1a9667dba9b5e4aaaa79cd1
levchmike <levchmike@gmail.com>
2022-10-28 03:32:23 +0700

    [BIO-39] test example for fuel type service

---
+++ BiotonFMS.Telematica.Tests/Mocks/FuelTypeMockData.cs
@@ -0,0 +1,23 @@
+﻿using BioTonFMS.Domain;
+
+namespace BiotonFMS.Telematica.Tests.Mocks
+{
+    public class FuelTypeMockData
+    {
+        public static IEnumerable<FuelType> GetFuelTypes()
+        {
+            return new List<FuelType>{
+                 new FuelType
+                 {
+                     Id = 1,
+                     Name = "Бензин"
+                 },
+                 new FuelType
+                 {
+                     Id = 1,
+                     Name = "Дизельное топливо"
+                 }
+            };
+        }
+    }
+}
