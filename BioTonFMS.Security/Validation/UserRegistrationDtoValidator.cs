ce62bfc22d7abab864192e137dea1f5ff833bcc2
Dmitry Anchikov <danchikov@gmail.com>
2022-11-18 17:31:12 +0400

    [BIO-59] [/api/auth/register] Добавить валидацию фио

--- BioTonFMS.Security/Validation/UserRegistrationDtoValidator.cs
+++ BioTonFMS.Security/Validation/UserRegistrationDtoValidator.cs
@@ -7,10 +7,10 @@ public class UserRegistrationDtoValidator : AbstractValidator<UserRegistrationDt
 {
     public UserRegistrationDtoValidator()
     {
-        RuleFor(x => x.FirstName).NotEmpty();
-        RuleFor(x => x.LastName).NotEmpty();
-        RuleFor(x => x.FirstName).NotEmpty();
-        RuleFor(x => x.UserName).NotEmpty().Length(3, 16);
+        RuleFor(x => x.FirstName).NotEmpty().MaximumLength(50).Matches("^[А-Яа-яa-zA-Z]*$").WithMessage("Имя пользователя имеет неверный формат, поле допускает использование русских и латинских букв");
+        RuleFor(x => x.LastName).NotEmpty().MaximumLength(50).Matches("^[А-Яа-яa-zA-Z]*$").WithMessage("Фамилия имеет неверный формат, поле допускает использование русских и латинских букв");
+        RuleFor(x => x.MiddleName).MaximumLength(50).Matches("^[А-Яа-яa-zA-Z]*$").WithMessage("Отчество имеет неверный формат, поле допускает использование русских и латинских букв");
+        RuleFor(x => x.UserName).NotEmpty().MaximumLength(20).Matches("^[А-Яа-яa-zA-Z1-9-_]*$").WithMessage("Логин имеет неверный формат, поле допускает использование русских и латинских букв, цифр, символов '-' и '_'");
         RuleFor(x => x.Password).NotEmpty();
     }
 }
