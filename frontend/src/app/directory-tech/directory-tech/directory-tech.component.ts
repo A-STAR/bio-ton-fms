0b38aa03ceb922e45008ff882efe8a909973663f
A-STAR <andreysuperstar@gmail.com>
2023-01-29 09:59:25 +0300

    [BIO-119] feat(frontend): add Directory tech page
    Add `DirectoryTechComponent` to directory tech routing.

--- frontend/src/app/directory-tech/directory-tech/directory-tech.component.ts
+++ frontend/src/app/directory-tech/directory-tech/directory-tech.component.ts
@@ -1,10 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'bio-directory-tech',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './directory-tech.component.html',
   styleUrls: ['./directory-tech.component.sass'],
   changeDetection: ChangeDetectionStrategy.OnPush
