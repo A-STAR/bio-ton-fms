941a73cafcabb0c4cbd74180d37a1700603a519d
A-STAR <andreysuperstar@gmail.com>
2023-01-30 15:25:57 +0300

    feat(frontend): add directory tech navigation
    Add `DirectoryTechComponent` navigation items.
    
    Test `DirectoryTechComponent` navigation rendering.
    
    Exclude `src/app/directory.routes.ts` file from code coverage.
    
    [BIO-119]

--- frontend/src/app/directory-tech/directory-tech/directory-tech.component.ts
+++ frontend/src/app/directory-tech/directory-tech/directory-tech.component.ts
@@ -1,16 +1,38 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'bio-directory-tech',
   standalone: true,
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MatButtonModule
   ],
   templateUrl: './directory-tech.component.html',
   styleUrls: ['./directory-tech.component.sass'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export default class DirectoryTechComponent { }
+export default class DirectoryTechComponent {
+  protected get navigation() {
+    return navigation;
+  }
+}
+
+type NavigationItem = {
+  title: string;
+  link: string;
+}
+
+export const navigation: NavigationItem[] = [
+  {
+    title: 'Машины',
+    link: 'vehicles'
+  },
+  {
+    title: 'GPS-трекеры',
+    link: 'trackers'
+  }
+];
