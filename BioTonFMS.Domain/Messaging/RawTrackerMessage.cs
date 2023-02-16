deede0ec4695079ee6e29b2a9389fd27fe7f605a
Dmitry Anchikov <danchikov@gmail.com>
2023-02-16 12:43:24 +0400

    Fix Warning in RawTrackerMessage

--- BioTonFMS.Domain/Messaging/RawTrackerMessage.cs
+++ BioTonFMS.Domain/Messaging/RawTrackerMessage.cs
@@ -6,9 +6,9 @@ public class RawTrackerMessage
     /// тип протокола
     /// </summary>
     public TrackerTypeEnum TrackerType { get; set; }
-    
+
     /// <summary>
     /// сообщение от трекера
     /// </summary>
-    public byte[] RawMessage { get; set; }
+    public byte[] RawMessage { get; set; } = null!; 
 }
