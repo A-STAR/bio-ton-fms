93f252749bb2e8f1dcdd46b002d3fd5c530d8f57
grin <backenddeveloper2.vpp@gmail.com>
2023-02-21 13:38:46 +0300

    [BIO-127] Fix mistakes related to tracker

--- BioTonFMS.Telematica/Validation/CreateTrackerDtoValidator.cs
+++ BioTonFMS.Telematica/Validation/CreateTrackerDtoValidator.cs
@@ -9,8 +9,10 @@ namespace BioTonFMS.Telematica.Validation
         {
             RuleFor(x => x.ExternalId).NotEmpty();
             RuleFor(x => x.Name).NotEmpty().Length(1, 100);
+            RuleFor(x => x.Imei).NotEmpty();
             RuleFor(x => x.StartDate).NotEmpty();
             RuleFor(x => x.SimNumber).NotEmpty().Length(1, 12);
+            RuleFor(x => x.StartDate).NotEmpty();
             When(x => x.Description != string.Empty, () => RuleFor(x => x.Description).Length(1, 500));
         }
     }
