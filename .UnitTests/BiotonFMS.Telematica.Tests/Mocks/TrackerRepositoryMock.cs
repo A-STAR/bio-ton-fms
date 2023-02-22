b71214bfccadc0aa37f2ebad35be24cfa8622fc6
grin <backenddeveloper2.vpp@gmail.com>
2023-02-22 19:33:26 +0300

    [BIO-39] Add tracker controller tests

--- .UnitTests/BiotonFMS.Telematica.Tests/Mocks/TrackerRepositoryMock.cs
+++ .UnitTests/BiotonFMS.Telematica.Tests/Mocks/TrackerRepositoryMock.cs
@@ -10,6 +10,7 @@ public static class TrackerRepositoryMock
 {
     public const int NonExistentTrackerId = -1;
     public const int ExistentTrackerId = 1;
+
     private static PagedResult<Tracker> GetTrackers() =>
         new()
         {
@@ -18,11 +19,36 @@ public static class TrackerRepositoryMock
             {
                 new()
                 {
-                    Id = 1
+                    Id = 1,
+                    Name = "трекер GalileoSky",
+                    Description = "Описание 1",
+                    Imei = "12341",
+                    ExternalId = 111,
+                    StartDate = DateTime.MinValue,
+                    TrackerType = TrackerTypeEnum.GalileoSkyV50,
+                    SimNumber = "905518101010"
                 },
                 new()
                 {
-                    Id = 2
+                    Id = 2,
+                    Name = "трекер Retranslator",
+                    Description = "Описание 2",
+                    Imei = "12342",
+                    ExternalId = 222,
+                    StartDate = DateTime.UnixEpoch,
+                    TrackerType = TrackerTypeEnum.Retranslator,
+                    SimNumber = "905518101020"
+                },
+                new()
+                {
+                    Id = 3,
+                    Name = "трекер WialonIPS",
+                    Description = "Описание 3",
+                    Imei = "12343",
+                    ExternalId = 333,
+                    StartDate = DateTime.MaxValue,
+                    TrackerType = TrackerTypeEnum.WialonIPS,
+                    SimNumber = "905518101030"
                 }
             }
         };
