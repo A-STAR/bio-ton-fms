2aae6702d858fcffe0cfa09a93d35e4fd153595e
grin <backenddeveloper2.vpp@gmail.com>
2023-03-13 17:59:11 +0300

    [BIO-127] Fix swagger, validation & mapping

--- BioTonFMS.Telematica/Validation/CreateTrackerDtoValidator.cs
+++ BioTonFMS.Telematica/Validation/CreateTrackerDtoValidator.cs
@@ -10,7 +10,7 @@ namespace BioTonFMS.Telematica.Validation
             RuleFor(x => x.ExternalId).NotEmpty();
             RuleFor(x => x.Name).NotEmpty().Length(1, 100);
             RuleFor(x => x.Imei).NotEmpty();
-            RuleFor(x => x.StartDate).NotEmpty();
+            RuleFor(x => x.TrackerType).NotEmpty();
             RuleFor(x => x.SimNumber).NotEmpty().Length(1, 12);
             When(x => x.Description != string.Empty, () => RuleFor(x => x.Description).Length(1, 500));
         }
