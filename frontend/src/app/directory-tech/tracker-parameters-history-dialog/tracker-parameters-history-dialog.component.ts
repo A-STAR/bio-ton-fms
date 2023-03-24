2511301356282037dd0813aebae7f47a62113d19
A-STAR <andreysuperstar@gmail.com>
2023-03-24 22:00:07 +0300

    feat(frontend): set parameters history data source
    Introduce `TrackerParametersHistoryDialog` `ParameterHistoryDataSource`
    interface.
    
    Map `TrackerParametersHistoryDialog`
    `TableDataSource<ParameterHistoryDataSource>`.
    
    BIO-172

--- frontend/src/app/directory-tech/tracker-parameters-history-dialog/tracker-parameters-history-dialog.component.ts
+++ frontend/src/app/directory-tech/tracker-parameters-history-dialog/tracker-parameters-history-dialog.component.ts
@@ -2,9 +2,11 @@ import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/cor
 import { CommonModule } from '@angular/common';
 import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
-import { Tracker, TrackerParametersHistory, TrackerService } from '../tracker.service';
+import { Tracker, TrackerParameterHistory, TrackerParametersHistory, TrackerService } from '../tracker.service';
+
+import { TableDataSource } from '../shared/table/table.data-source';
 
 @Component({
   selector: 'bio-tracker-parameters-history-dialog',
@@ -19,14 +21,51 @@ import { Tracker, TrackerParametersHistory, TrackerService } from '../tracker.se
 })
 export class TrackerParametersHistoryDialogComponent implements OnInit {
   protected parameters$!: Observable<TrackerParametersHistory>;
+  protected parametersDataSource!: TableDataSource<ParameterHistoryDataSource>;
+
+  /**
+   * Map parameters history data source.
+   *
+   * @param parameters Parameters history.
+   *
+   * @returns Mapped parameters history data source.
+   */
+  #mapParametersDataSource(parameters: TrackerParameterHistory[]) {
+    return Object
+      .freeze(parameters)
+      .map(({ time, speed, latitude, longitude, altitude, parameters }): ParameterHistoryDataSource => ({
+        time,
+        speed,
+        coordinates: { latitude, longitude },
+        altitude,
+        parameters
+      }));
+  }
+
+  /**
+   * Initialize `TableDataSource` with parameters data source.
+   *
+   * @param parameters Standard parameters.
+   */
+  #setParametersDataSource(parameters: TrackerParameterHistory[]) {
+    const parametersHistory = this.#mapParametersDataSource(parameters);
+
+    this.parametersDataSource = new TableDataSource<ParameterHistoryDataSource>(parametersHistory);
+  }
 
   /**
    * Get and set tracker parameters history.
    */
   #setParameters() {
-    this.parameters$ = this.trackerService.getParametersHistory({
-      trackerId: this.data
-    });
+    this.parameters$ = this.trackerService
+      .getParametersHistory({
+        trackerId: this.data
+      })
+      .pipe(
+        tap(({ parameters }) => {
+          this.#setParametersDataSource(parameters);
+        })
+      );
   }
 
   constructor(@Inject(MAT_DIALOG_DATA) protected data: Tracker['id'], private trackerService: TrackerService) { }
@@ -34,5 +73,12 @@ export class TrackerParametersHistoryDialogComponent implements OnInit {
   // eslint-disable-next-line @typescript-eslint/member-ordering
   ngOnInit() {
     this.#setParameters();
+  }
+}
+
+interface ParameterHistoryDataSource extends Pick<TrackerParameterHistory, 'time' | 'speed' | 'altitude' | 'parameters'> {
+  coordinates: {
+    latitude: TrackerParameterHistory['latitude'];
+    longitude: TrackerParameterHistory['longitude'];
   }
 }
