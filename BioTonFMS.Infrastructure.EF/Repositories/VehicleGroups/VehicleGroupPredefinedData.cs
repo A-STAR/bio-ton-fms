5ce15fc1c23f9c462f9680ef94b05dd0c396b0ca
Denis Kerzhemanov <backenddeveloper1.vpp@gmail.com>
2023-04-01 20:30:28 +0000

    BIO-177 Слияние предыдущих миграций BioTonDBContext + Bugfixing

---
+++ BioTonFMS.Infrastructure.EF/Repositories/VehicleGroups/VehicleGroupPredefinedData.cs
@@ -0,0 +1,21 @@
+﻿using BioTonFMS.Domain;
+
+namespace BioTonFMS.Infrastructure.EF.Repositories.VehicleGroups;
+
+public static class VehicleGroupPredefinedData
+{
+    public static readonly IEnumerable<VehicleGroup> VehicleGroups = new[]
+    {
+        new VehicleGroup()
+        {
+            Id = 1,
+            Name = "Группа 1"
+        },
+        new VehicleGroup()
+        {
+            Id = 2,
+            Name = "Группа 2"
+        }
+    };
+}
+
