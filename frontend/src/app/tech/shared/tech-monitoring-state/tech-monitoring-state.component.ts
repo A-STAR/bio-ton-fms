022ac7f2fc6f7193156d4de6e8732fa5eba2b4af
A-STAR <andreysuperstar@gmail.com>
2023-07-18 03:39:38 +0300

    BIO-224 feat(frontend): add monitoring icons state
    Pass `bio-tech` vehicle to `bio-tech-monitoring-state`.
    
    Add `bio-tech-monitoring-state` icons state.
    
    Style connection `mat-icon` `color`.
    
    Test `bio-tech-monitoring-state` icons state, color.

--- frontend/src/app/tech/shared/tech-monitoring-state/tech-monitoring-state.component.ts
+++ frontend/src/app/tech/shared/tech-monitoring-state/tech-monitoring-state.component.ts
@@ -1,7 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+
+import { ConnectionStatus, MonitoringVehicle, MovementStatus } from '../../tech.service';
 
 import { StopClickPropagationDirective } from 'src/app/shared/stop-click-propagation/stop-click-propagation.directive';
 
@@ -18,4 +20,9 @@ import { StopClickPropagationDirective } from 'src/app/shared/stop-click-propaga
   styleUrls: ['./tech-monitoring-state.component.sass'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TechMonitoringStateComponent { }
+export class TechMonitoringStateComponent {
+  @Input() vehicle!: MonitoringVehicle;
+
+  protected MovementStatus = MovementStatus;
+  protected ConnectionStatus = ConnectionStatus;
+}
