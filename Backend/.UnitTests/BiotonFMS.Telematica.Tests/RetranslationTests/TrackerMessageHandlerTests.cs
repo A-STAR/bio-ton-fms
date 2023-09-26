0838a625568e693e26f50e75824c8c6f839a912d
Dmitry Anchikov <danchikov@gmail.com>
2023-09-26 21:53:21 +0400

    BIO-245 Переделать тест TrackerMessageHandler_ShouldEnqueueRawMessageForRetranslation

--- Backend/.UnitTests/BiotonFMS.Telematica.Tests/RetranslationTests/TrackerMessageHandlerTests.cs
+++ Backend/.UnitTests/BiotonFMS.Telematica.Tests/RetranslationTests/TrackerMessageHandlerTests.cs
@@ -14,18 +14,27 @@ using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
+using BioTonFMS.Infrastructure.EF.Repositories.Trackers;
+using BioTonFMS.Infrastructure.EF.Repositories.TrackerTags;
 
 namespace BiotonFMS.Telematica.Tests.RetranslationTests;
 
 public class TrackerMessageHandlerTests
 {
-    /*[Fact]
+    [Fact]
     public async Task TrackerMessageHandler_ShouldEnqueueRawMessageForRetranslation()
     {
+        var logStub = new Mock<ILogger<TrackerMessageHandler>>();
+        var codecProvider = (Func<TrackerTypeEnum, ICommandCodec>)(_ => new GalileoskyCommandCodec());
         var retBus = new MessageBusMock();
         var consBus = new MessageBusMock();
         var commandReceiveBus = new MessageBusMock();
-        var testData = new byte[] { 0, 1, 2, 3, 4, 5, 6 };
+        var trackerMessageRepository = new Mock<ITrackerMessageRepository>();
+        var trackerTagRepository = new Mock<ITrackerTagRepository>();
+        var trackerRepository = new Mock<ITrackerRepository>();
+
+        var testData = new byte[] { 0x01, 0x17, 0x80, 0x01, 0x82, 0x02, 0x17, 0x03, 0x38, 0x36, 0x32, 0x30, 0x35, 0x37, 0x30, 
+            0x34, 0x37, 0x38, 0x33, 0x32, 0x38, 0x32, 0x36, 0x04, 0xCC, 0x07, 0x4A, 0x22 };
         var parserProviderMock = (Func<TrackerTypeEnum, ITrackerMessageParser>)(_ => new GalileoskyMessageParser(ProtocolTagRepositoryMock.GetStub(), Mock.Of<ILogger<GalileoskyMessageParser>>()));
         var busResolver = (Func<MessgingBusType, IMessageBus>)(busType => busType switch
             {
@@ -34,24 +43,17 @@ public class TrackerMessageHandlerTests
                 MessgingBusType.TrackerCommandsReceive => commandReceiveBus,
                 _ => throw new ArgumentOutOfRangeException(nameof(busType), busType, null)
             });
-        var codecResolver = (Func<TrackerTypeEnum, ICommandCodec>)(_ => new GalileoskyCommandCodec());
-        var handler = new TrackerMessageHandler(
-            Mock.Of<ILogger<TrackerMessageHandler>>(), 
-            parserProviderMock,
-            codecResolver,
-            Mock.Of<ITrackerMessageRepository>(),
-            TrackerTagRepositoryMock.GetStub(),
-            TrackerRepositoryMock.GetStub(),
-            busResolver,
-            Options.Create(new RetranslatorOptions { Enabled = true })
-        );
+
+        var trackerMessageHandler = new TrackerMessageHandler(logStub.Object, parserProviderMock, codecProvider,
+            trackerMessageRepository.Object, trackerTagRepository.Object, trackerRepository.Object,
+            busResolver, Options.Create(new RetranslatorOptions { Enabled = true }));
 
         var rawMsgEncoded = Encoding.UTF8
             .GetBytes(JsonSerializer.Serialize(new RawTrackerMessage { RawMessage = testData }));
 
         try
         {
-            await handler.HandleAsync(rawMsgEncoded);
+            await trackerMessageHandler.HandleAsync(rawMsgEncoded, deliveryTag: 0);
         }
         catch(Exception)
         {
@@ -59,5 +61,5 @@ public class TrackerMessageHandlerTests
 
         retBus.Messages.Count.Should().Be(1);
         retBus.Messages[0].Should().Equal(rawMsgEncoded);
-    }*/
+    }
 }
