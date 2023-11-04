40dbdd4644f78f98274695fabae9896f7f3a362c
A-STAR <andreysuperstar@gmail.com>
2023-11-04 10:13:53 +0300

    BIO-283 feat(frontend): add map to Messages page
    Add `bio-map` to Messages page. Style `bio-map`.
    
    Test `bio-messages` `bio-map` component rendering.

--- frontend/src/app/messages/messages.component.ts
+++ frontend/src/app/messages/messages.component.ts
@@ -1,10 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MapComponent } from '../shared/map/map.component';
+
 @Component({
   selector: 'bio-messages',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MapComponent
+  ],
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.sass'],
   changeDetection: ChangeDetectionStrategy.OnPush
