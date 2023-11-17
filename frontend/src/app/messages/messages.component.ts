8cac28f494ddc516c9835d170db8aef310c9f30b
A-STAR <andreysuperstar@gmail.com>
2023-11-17 08:06:38 +0300

    feat(frontend): add message range time selection
    Add `bio-messages` `fieldset` with `range` `start`, `end` `time` selection fields.
    
    Style `bio-messages` `form` `start`, `end` `fieldset` fields layout.
    
    Test `bio-messages` selection `form` `range` `time` fields rendering.
    
    BIO-289

--- frontend/src/app/messages/messages.component.ts
+++ frontend/src/app/messages/messages.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
